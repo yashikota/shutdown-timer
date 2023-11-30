3fac363a5555de06eaf664a826d908e630d7c528
kota <52403688+yashikota@users.noreply.github.com>
2023-11-30 23:22:07 +0900

    update window size

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,20 +1,9 @@
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using Windows.Graphics;
 
 namespace shutdown_timer
 {
@@ -27,7 +16,15 @@ namespace shutdown_timer
 		{
 			InitializeComponent();
 
+			// 背景をMicaデザインに
 			SystemBackdrop = new MicaBackdrop();
+
+			// ウィンドウサイズの調整
+			IntPtr hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+			WindowId windowId = Win32Interop.GetWindowIdFromWindow(hWnd);
+			AppWindow appWindow = AppWindow.GetFromWindowId(windowId);
+
+			appWindow.Resize(new SizeInt32(800, 600));
 		}
 	}
 }
